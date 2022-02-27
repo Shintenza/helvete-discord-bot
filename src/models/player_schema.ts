90ef4d70967d48d3a5fc91d5e7b98f0e12ea5b6b
Shintenza <kam.kuziora@gmail.com>
2022-02-27 23:50:41 +0100

    loading player image from .env file

--- src/models/player_schema.ts
+++ src/models/player_schema.ts
@@ -4,12 +4,12 @@ class Player extends MessageEmbed {
     constructor(
         title = 'Helvete music player',
         description = 'I am playing nothing now',
-        imageUrl = 'https://media.discordapp.net/attachments/669893606651199488/880097037473964082/Helveteplus_to2.png?width=633&height=475'
+        imageUrl = process.env.PLAYER_URL
     ) {
         super();
         this.setTitle(title);
         this.setDescription(description);
-        this.setImage(imageUrl);
+        this.setImage(imageUrl!);
     }
 }
 export = Player;
